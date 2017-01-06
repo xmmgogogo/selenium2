930a8662800ca85a3d2c4dca58649f3726b8b3d2
mianyamian <liumianjun@gmail.com>
2017-01-06 21:29:53 +0800

    测试网站

--- baidu.py
+++ baidu.py
@@ -3,8 +3,20 @@ __author__ = 'Administrator'
 # coding=utf-8
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.
 
 driver = webdriver.Firefox()
 driver.get("http://www.baidu.com")
+driver.find_element_by_id('kw').send_keys('selenium')
+driver.find_element_by_id('su').click()
 
-driver.quit()
+print(driver.title)
+
+WebDriverWait(driver, 10).until(EC.title_contains("selenium"))
+
+# You should see "cheese! - Google Search"
+print driver.title
+
+# driver.quit()
